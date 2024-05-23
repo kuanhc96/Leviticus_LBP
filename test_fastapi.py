89a0d42b36fe83df00e00340b324a7bc84f4fcfe
kuanhc96 <kuanhc96@uchicago.edu>
2024-05-23 22:12:54 +0800

    use compare directory from toolbox

--- test_fastapi.py
+++ test_fastapi.py
@@ -284,20 +284,3 @@ def predict(request: LBPPredictRequest) -> dict:
             "classificationReport": None,
             "predictions": dict(zip(imageNames, predictions.tolist()))
         }
-
-# Check to see if the directory for predict has the same structure of
-# the directory that was provided for training
-def _isEqualSubDirs(dir1, dir2):
-    dir1SubDirs = next(os.walk(dir1))[1]
-    dir2SubDirs = next(os.walk(dir2))[1]
-
-    if len(dir1SubDirs) != len(dir2SubDirs):
-        return False
-
-    dir1SubDirs.sort()
-    dir2SubDirs.sort()
-
-    for (subDir1, subDir2) in zip(dir1SubDirs, dir2SubDirs):
-        if subDir1 != subDir2:
-            return False
-    return True
